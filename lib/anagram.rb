97e2e90fe05daf7db00b1fa53a29526284c2066f
kevinkkimutai <kiruikevinkim@gmail.com>
2023-02-09 11:06:51 +0300

    Assignment completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(words)
+      words.select do |w|
+        next if w.downcase == @word.downcase
+        w.downcase.chars.sort == @word.downcase.chars.sort
+      end
+    end
+  end
+  
+  detector = Anagram.new("listen")
+  p detector.match(["enlists", "google", "inlets", "banana"])
+  
